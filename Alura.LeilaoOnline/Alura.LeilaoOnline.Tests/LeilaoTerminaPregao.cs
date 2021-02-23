6ce15bc319d2c3d96e94a1f3a72e26bca5e6edb6
Douglas Tertuliano <douglasjtds@gmail.com>
2021-02-23 16:59:47 -0300

    some refactorings

---
+++ Alura.LeilaoOnline/Alura.LeilaoOnline.Tests/LeilaoTerminaPregao.cs
@@ -0,0 +1,48 @@
+using Alura.LeilaoOnline.Core;
+using Xunit;
+
+namespace Alura.LeilaoOnline.Tests
+{
+    public class LeilaoTerminaPregao
+    {
+        [Theory]
+        [InlineData(1200, new double[] { 800, 900, 1000, 1200 })]
+        [InlineData(1000, new double[] { 800, 900, 1000, 990 })]
+        [InlineData(800, new double[] { 800 })]
+        public void RetornaMaiorValorDadoLeilaoComPeloMenosUmLance(double valorEsperado, double[] ofertas)
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+            var fulano = new Interessada("Fulano", leilao);
+
+            //Act
+            foreach (var valor in ofertas)
+            {
+                leilao.RecebeLance(fulano, valor);
+            }
+
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorObtido = leilao.Ganhador.Valor;
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+
+        
+        [Fact]
+        public void RetornaZeroDadoLeilaoSemLances()
+        {
+            //Arrange
+            var leilao = new Leilao("Van Gogh");
+
+            //Act
+            leilao.TerminaPregao();
+
+            //Assert
+            var valorEsperado = 0;
+            var valorObtido = leilao.Ganhador.Valor;
+
+            Assert.Equal(valorEsperado, valorObtido);
+        }
+    }
+}
